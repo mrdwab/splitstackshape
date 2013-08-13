577484129378e8a969ba148907af0680c2f47537
Ananda Mahto <mrdwab@gmail.com>
2013-08-13 08:13:14 +0530

    removed assign for CRAN compatibility

--- R/splitstackshape_helper_functions.R
+++ R/splitstackshape_helper_functions.R
@@ -189,8 +189,6 @@ NULL
 #' 
 #' 
 #' @param mydf The name of your \code{data.frame}
-#' @param overwrite Logical. Should the current object be overwritten? Defaults
-#' to \code{FALSE}
 #' @author Ananda Mahto
 #' @seealso \code{\link{read.table}}
 #' @examples
@@ -199,25 +197,16 @@ NULL
 #' dat <- data.frame(title = c("title1", "title2", "title3"),
 #'          author = c("author1", "author2", "author3"),
 #'          customerID = c(1, 2, 1))
-#' ## Make a copy of dat for later
-#' dat_copy <- dat
+#'          
 #' str(dat) # current structure
 #' dat2 <- splitstackshape:::FacsToChars(dat)
 #' str(dat2) # Your new object
 #' str(dat)  # Original object is unaffected
 #' 
-#' ## You can also overwrite the existing object
-#' str(dat_copy) # Before applying the function
-#' splitstackshape:::FacsToChars(dat_copy, overwrite=TRUE)
-#' str(dat_copy) # After applying the function
 #' \dontshow{rm(dat, dat2, dat_copy)}
 #' 
-FacsToChars <- function(mydf, overwrite = FALSE) {
-  dfname <- deparse(substitute(mydf))
+FacsToChars <- function(mydf) {
   mydf[sapply(mydf, is.factor)] <- 
     lapply(mydf[sapply(mydf, is.factor)], as.character)
-  if (isTRUE(overwrite)) {
-    assign(dfname, mydf, envir = .GlobalEnv)
-  } 
   mydf
 }

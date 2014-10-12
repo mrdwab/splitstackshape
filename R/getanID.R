e02e5a4368325545537126f509b8feeab4968730
Ananda Mahto <mrdwab@gmail.com>
2014-10-12 22:34:43 +0530

    getanID updated

--- R/getanID.R
+++ R/getanID.R
@@ -1,16 +1,14 @@
 #' Add an "id" variable to a dataset
 #' 
 #' Many functions will not work properly if there are duplicated ID variables
-#' in a dataset. This function is a convenience function for using
-#' \code{\link{ave}} and \code{\link{seq_along}} to create an \code{.id}
+#' in a dataset. This function is a convenience function for \code{.N} from the "data.table" package to create an "\code{.id}"
 #' variable that when used in conjunction with the existing ID variables,
 #' should be unique.
 #' 
 #' 
-#' @param data The input \code{data.frame}.
-#' @param id.vars The variables that should be treated as ID variables.
-#' @return The input \code{data.frame} if ID variables are unique or the input
-#' \code{data.frame} with a new \code{.id} column.
+#' @param data The input \code{data.frame} or \code{data.table}.
+#' @param id.vars The variables that should be treated as ID variables. Defaults to \code{NULL}, at which point all variables are used to create the new ID variable.
+#' @return The input dataset (as a \code{data.table}) if ID variables are unique, or the input dataset with a new column named "\code{.id}".
 #' @author Ananda Mahto
 #' @examples
 #' 
@@ -27,11 +25,15 @@
 #' \dontshow{rm(mydf)}
 #' 
 #' @export getanID
-getanID <- function(data, id.vars) {
-  x <- do.call(paste0, data[id.vars])
-  if (any(duplicated(x))) {
-    data[[".id"]] <- ave(x, x, FUN = seq_along)
-    data
+getanID <- function(data, id.vars = NULL) {
+  if (!is.data.table(data)) data <- as.data.table(data)
+  else data <- copy(data)
+  
+  if (is.numeric(id.vars)) id.vars <- names(data)[id.vars]
+  if (is.null(id.vars)) id.vars <- names(data)
+  
+  if (any(duplicated(data, by = id.vars))) {
+    data[, .id := sequence(.N), by = id.vars]
   } else {
     data
   }

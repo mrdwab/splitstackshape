7a7de0bfae95e6e70d7bd96325b8eca4fe8eb65a
Ananda Mahto <mrdwab@gmail.com>
2018-03-29 09:29:29 +0530

    getanID added with tests

--- R/getanID.R
+++ R/getanID.R
@@ -1,14 +1,14 @@
 #' Add an "id" Variable to a Dataset
 #' 
-#' Many functions will not work properly if there are duplicated ID variables
-#' in a dataset. This function is a convenience function for \code{.N} from the "data.table" package to create an "\code{.id}"
-#' variable that when used in conjunction with the existing ID variables,
-#' should be unique.
+#' Many functions will not work properly if there are duplicated ID variables in 
+#' a dataset. This function is a convenience function for [data.table::rowid()] 
+#' and is largely unnecessary now that `rowid` exists.
 #' 
-#' 
-#' @param data The input \code{data.frame} or \code{data.table}.
-#' @param id.vars The variables that should be treated as ID variables. Defaults to \code{NULL}, at which point all variables are used to create the new ID variable.
-#' @return The input dataset (as a \code{data.table}) if ID variables are unique, or the input dataset with a new column named "\code{.id}".
+#' @param data The input `data.table`.
+#' @param id.vars The variables that should be treated as ID variables. Defaults 
+#' to `NULL`, at which point all variables are used to create the new ID variable.
+#' @return The input dataset if ID variables are already unique, or the input 
+#' dataset with a new column named "`sub_id`".
 #' @author Ananda Mahto
 #' @examples
 #' 
@@ -22,21 +22,18 @@
 #' mydf
 #' getanID(mydf, 1:2)
 #' 
-#' \dontshow{rm(mydf)}
-#' 
 #' @export getanID
-getanID <- function(data, id.vars = NULL) {
-  if (!is.data.table(data)) data <- as.data.table(data)
-  else data <- copy(data)
+getanID <- function(indt, id.vars = NULL) {
+  indt <- setDT(copy(indt))
+  if (is.null(id.vars)) id.vars <- names(indt)
+  if (is.numeric(id.vars)) id.vars <- names(indt)[id.vars]
   
-  if (is.numeric(id.vars)) id.vars <- names(data)[id.vars]
-  if (is.null(id.vars)) id.vars <- names(data)
+  sub_id <- NULL
   
-  .id <- .N <- NULL
-  
-  if (any(duplicated(data, by = id.vars))) {
-    data[, .id := sequence(.N), by = id.vars]
+  if (any(duplicated(indt, by = id.vars))) {
+    indt[, sub_id := rowidv(.SD), .SDcols = id.vars][]
   } else {
-    data
+    indt[]
   }
 }
+NULL
